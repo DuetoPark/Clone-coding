4b086f0e12b1bba53ae3821c8948e8bf3c7cce54
DuetoPark <ffdddffddd11@gmail.com>
2021-01-15 18:46:51 +0900

    BUG: serialization파일 .serialization-webtoon 출력/숨김 수정 -serialization.js

--- 02-lezhin-comics/assets/serialization.js
+++ 02-lezhin-comics/assets/serialization.js
@@ -1,45 +1,70 @@
 // Serialization.html
 // Button Click Event - Nav.serialization-nav
+const weekButton = document.querySelectorAll("button.week-button.type");
 const weekButtonlist = document.querySelector(".serialization-nav .type-list");
 const dailyWebtoon = document.querySelectorAll(".serialization-webtoon");
 
-function changeList (target) {
-  for (let i=0; i<dailyWebtoon.length; i++){
-    dailyWebtoon[i].style.display = "none";
+let today = new Date();
+let mm = today.getDay();
+let selected = "";
+
+for (let i=0; i<dailyWebtoon.length; i++){
+  dailyWebtoon[i].style.display = "none";
+}
+
+for (let i=0; i<weekButton.length; i++) {
+  if(Number(weekButton[i].value) === mm){
+    weekButton[i].classList.add("type-active");
+    dailyWebtoon[i].style.display = 'block';
   }
+}
+
+
+function changeList (target) {
+  dailyWebtoon[mm-1].style.display = "none";
+  weekButton[mm-1].classList.remove("type-active");
 
   switch (target.value) {
-    case "mon":
-    dailyWebtoon[0].style.display = "block";
+    case "1":
+    dailyWebtoon[target.value-1].style.display = "block";
+    weekButton[target.value-1].classList.add("type-active");
     break;
 
-    case "tue":
-    dailyWebtoon[1].style.display = "block";
+    case "2":
+    dailyWebtoon[target.value-1].style.display = "block";
+    weekButton[target.value-1].classList.add("type-active");
     break;
 
-    case "wed":
-    dailyWebtoon[2].style.display = "block";
+    case "3":
+    dailyWebtoon[target.value-1].style.display = "block";
+    weekButton[target.value-1].classList.add("type-active");
     break;
 
-    case "thu":
-    dailyWebtoon[3].style.display = "block";
+    case "4":
+    dailyWebtoon[target.value-1].style.display = "block";
+    weekButton[target.value-1].classList.add("type-active");
     break;
 
-    case "fri":
-    dailyWebtoon[4].style.display = "block";
+    case "5":
+    dailyWebtoon[target.value-1].style.display = "block";
+    weekButton[target.value-1].classList.add("type-active");
     break;
 
-    case "sat":
-    dailyWebtoon[5].style.display = "block";
+    case "6":
+    dailyWebtoon[target.value-1].style.display = "block";
+    weekButton[target.value-1].classList.add("type-active");
     break;
+  }
+  mm = target.value;
 
-    case "sun":
+  if (target.value === "0") {
     dailyWebtoon[6].style.display = "block";
-    break;
-
-    case "10days":
+    weekButton[6].classList.add("type-active");
+    mm = 7;
+  } else if (target.value === "7") {
     dailyWebtoon[7].style.display = "block";
-    break;
+    weekButton[7].classList.add("type-active");
+    mm = 8;
   }
 }
 
