2aa46369161fec4f07254f14a5e4e3357f54dc1d
DuetoPark <ffdddffddd11@gmail.com>
2021-01-22 00:25:11 +0900

    BUG: .login-modal 클릭 이벤트 수정 -modal.js
    -addEventListener 적용 대상 교체: button.login-button에서 nav.longin-modal(button.login-button의 부모요소)로 변경
    -currentStatus 변수 추가: button.login-button 누르면 모달 열림/닫힘 둘 다 가능

--- 02-lezhin-comics/assets/modal.js
+++ 02-lezhin-comics/assets/modal.js
@@ -1,10 +1,10 @@
 // Modal event
 const loginButton = document.querySelector(".login-button");
-const closeModalButton = document.querySelector(".modal-close");
 const loginModal = document.querySelector(".login-modal");
+let currentStatus = 0;
 
 
-function openLoginWindow () {
+function loginButtonControl () {
   // 애니메이션 진행중에 .modal-active 클래스 추가
   // - 진행중에 클래스를 추가하는 이유
   //   : 프로그램은 작성 순서대로 진행되기 때문이다.
@@ -14,14 +14,20 @@ function openLoginWindow () {
   //   : 클래스 추가 {display: 1; opacity: 1;}
   //   공백일때 잠깐 지워지니까 동작 도중에 깜빡깜빡하는 현상이 나타남.
   //   setTimeout을 사용해서 애니메이션이 끝나기 전에 클래스를 추가하면 됨.
-  loginModal.classList.add("modal-open-animation");
-  document.body.classList.add("body-scroll-off");
-  window.setTimeout(function () {
-    loginModal.classList.add("modal-active");
-  }, 200);
-  window.setTimeout(function () {
-    loginModal.classList.remove("modal-open-animation");
-  }, 300);
+  if (currentStatus === 0) {
+    loginModal.classList.add("modal-open-animation");
+    document.body.classList.add("body-scroll-off");
+    window.setTimeout(function () {
+      loginModal.classList.add("modal-active");
+    }, 200);
+    window.setTimeout(function () {
+      loginModal.classList.remove("modal-open-animation");
+    }, 300);
+    currentStatus = 1;
+  } else if (currentStatus === 1){
+    closeLoginWindow();
+    currentStatus = 0;
+  }
 }
 
 function closeLoginWindow () {
@@ -33,15 +39,18 @@ function closeLoginWindow () {
   window.setTimeout(function () {
     loginModal.classList.remove("modal-close-animation");
   }, 300);
+  currentStatus = 0;
 }
 
-function openLoginHandler () {
-  openLoginWindow();
+function loginModalHandler () {
+  loginButtonControl();
 }
 
-function closeLoginHandler () {
-  closeLoginWindow();
+function closeLoginHandler (e) {
+  if (e.target.classList[0] === "modal-close") {
+    closeLoginWindow();
+  }
 }
 
-loginButton.addEventListener("click", openLoginHandler);
-closeModalButton.addEventListener("click", closeLoginHandler);
+loginButton.addEventListener("click", loginModalHandler);
+loginModal.addEventListener("click", closeLoginHandler);
