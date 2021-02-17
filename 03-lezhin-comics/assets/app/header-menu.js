ba4251cfc6e11089460e9041770142b6b13f021a
DuetoPark <ffdddffddd11@gmail.com>
2021-02-17 23:24:37 +0900

    DOC: 페이지 이동하는 링크 생성 -03-lezhin-comics/assets/app/header-menu.js

---
+++ 03-lezhin-comics/assets/app/header-menu.js
@@ -0,0 +1,20 @@
+// Header Menu - 페이지 이동하는 링크 생성
+// 연재, 로맨스, 소년, 드라마, bl 등등
+const menuUl = document.querySelector("#nav-menu");
+let menuKeys = Object.keys(state.메뉴);
+
+function createMenu () {
+  for (let i=0; i<menuKeys.length; i++) {
+    let menuList = document.createElement("li");
+    menuList.setAttribute("class", "menu-list-items");
+    menuList.setAttribute("role", "none");
+    menuUl.appendChild(menuList);
+
+    let menuItem = document.createElement("a");
+    menuItem.setAttribute("class", "menu-anchor");
+    menuItem.setAttribute("role", "menuitem");
+    menuItem.innerHTML = menuKeys[i];
+    menuList.appendChild(menuItem);
+  }
+};
+createMenu();
