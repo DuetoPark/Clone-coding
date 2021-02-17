dad0c8d9ac1c1d414dd2f6de3c225094334defed
DuetoPark <ffdddffddd11@gmail.com>
2021-02-17 23:27:50 +0900

    DOC: 메인-랭킹섹션-장르 버튼 생성 -03-lezhin-comics/assets/app/main-rank-genre.js

---
+++ 03-lezhin-comics/assets/app/main-rank-genre.js
@@ -0,0 +1,20 @@
+// Main ranking - 장르 버튼 생성
+// 전체, 로맨스, bl, 드라마, 판타지, 개그, 액션 등등
+const selectGenre = document.querySelector("#select-genre");
+let rankingKeys = Object.keys(state.랭킹);
+
+function createGenre () {
+  for (let i=0; i<rankingKeys.length; i++) {
+    let genreButton = document.createElement("button");
+    genreButton.setAttribute("type", "button");
+    genreButton.setAttribute("id", rankingKeys[i]+"-button");
+    genreButton.setAttribute("class", "genre-button");
+    genreButton.setAttribute("value", rankingKeys[i]);
+    genreButton.setAttribute("role", "tab");
+    genreButton.setAttribute("aria-controls", rankingKeys[i]+"-rank");
+    genreButton.innerHTML = rankingKeys[i];
+    selectGenre.appendChild(genreButton);
+  }
+};
+
+createGenre();
