7de23967365e2e4085ca6e1cdd8123baa46054ce
DuetoPark <ffdddffddd11@gmail.com>
2021-02-19 13:22:32 +0900

    DOC: 메인-배너섹션-배너조작버튼 함수 작성 -03-lezhin/assets/app/app

--- 03-lezhin-comics/assets/app/app.js
+++ 03-lezhin-comics/assets/app/app.js
@@ -1,9 +1,28 @@
+// Slick 슬라이더 라이브러리 선언
 $(document).ready(function(){
-  $('.banner-slider').slick({
+  $('#banner-slider').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     prevArrow: $(".prev"),
     nextArrow: $(".next"),
   });
+});
+
+
+// 배너 버튼
+const bannerController = document.querySelector(".banner-controller");
+let bodyWidth = window.innerWidth;
+
+if (bodyWidth>=640) {
+  bannerController.classList.remove("hidden");
+}
+
+$(window).resize(function(){
+  bodyWidth = window.innerWidth;
+  if (bodyWidth>=640) {
+    bannerController.classList.remove("hidden");
+  } else {
+    bannerController.classList.add("hidden");
+  }
 });
