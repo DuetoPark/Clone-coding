50664b594283fcc05bd6850f8d8b5f60ef5cd2d5
DuetoPark <ffdddffddd11@gmail.com>
2021-04-07 01:34:08 +0900

    MOD: 헤더-nav css 수정 -main-create-list
    -ol, li 태그 추가
    -li 태그에 flex-grow 추가

--- 02-lezhin-comics/assets/app/header-menu.js
+++ 02-lezhin-comics/assets/app/header-menu.js
@@ -1,15 +1,19 @@
 // Header Menu - 페이지 이동하는 링크 생성
 // 연재, 로맨스, 소년, 드라마, bl 등등
-const menuDiv = document.querySelector("#nav-menu");
+const menuOl = document.querySelector("#nav-menu");
 let menuKeys = Object.keys(state.메뉴);
 
 function createMenu () {
   for (let i=0; i<menuKeys.length; i++) {
+    let menuLi = document.createElement("li");
+    menuLi.className = "menu-list-items";
+    menuOl.appendChild(menuLi);
+
     let menuItem = document.createElement("a");
-    menuItem.setAttribute("class", "menu-anchor");
+    menuItem.className = "menu-anchor";
     menuItem.setAttribute("href", "./lezhin-home.html");
     menuItem.innerHTML = menuKeys[i];
-    menuDiv.appendChild(menuItem);
+    menuLi.appendChild(menuItem);
   }
 };
 createMenu();
